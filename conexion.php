b6cb38ffe1d361e1ed6dfcbef8605d240301554b
alejandracarabali <alejandra.carabali@correounivalle.edu.co>
2026-09-22 01:56:48 -0500

    Fix: correccion de contraseña MySQL

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 $host     = 'sql207.infinityfree.com';          // Host exacto de tu captura
 $dbname   = 'if0_42936396_tienda_promociones'; // BD exacta de tu captura
 $username = 'if0_42936396';                    // Tu usuario de InfinityFree
-$password = 'TU_CONTRASEÑA_DE_INFINITYFREE';   // La contraseña de tu cuenta
+$password = '949ct6THmM';   // La contraseña de tu cuenta
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
